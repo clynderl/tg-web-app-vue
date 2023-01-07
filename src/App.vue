1e71af9637e1d8a63408cb07b2654546aa30a875
Pavel Semenov <p.semenov@idaproject.com>
2023-01-07 22:47:28 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {onMounted} from 'vue';
+
+const tg = window.Telegram.WebApp;
+
+onMounted(() => {
+  tg.ready();
+});
+const onClose = () => {
+  tg.close();
+};
+</script>
+
+<template>
+  <div>
+    work
+    <button @click="onClose">Close</button>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
