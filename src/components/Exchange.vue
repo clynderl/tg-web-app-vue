ed258c0f22621577c27b3141808e4580833801c8
Pavel Semenov <p.semenov@idaproject.com>
2023-01-08 14:36:37 +0700

    init

--- src/components/Exchange.vue
+++ src/components/Exchange.vue
@@ -11,6 +11,7 @@ const fromActive = ref(null);
 
 <template>
   <div class="exchange">
+    <h1 class="title">1. Отдаете</h1>
     <Suspense>
       <template #default>
         <FromDirections :directions="directions" :active-item="fromActive" @change="fromActive = $event" />
@@ -29,5 +30,12 @@ const fromActive = ref(null);
   padding-bottom: 10px;
   background: var(--tg-theme-secondary-bg-color);
   border-radius: 10px;
+}
+
+.title {
+  font-size: 24px;
+  margin-bottom: 20px;
+  margin-left: 10px;
+  color: var(--tg-theme-text-color);
 }
 </style>
--- src/components/FromDirections.vue
+++ src/components/FromDirections.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1 class="title">1. Отдаете</h1>
   <DirectionsList :directions="items" @change="emit('change', $event)" :active-item="activeItem" />
 </template>
 
@@ -46,8 +45,5 @@ const emit = defineEmits(['change']);
 </script>
 
 <style scoped>
-.title {
-  margin-bottom: 20px;
-  color: var(--tg-theme-text-color);
-}
+
 </style>
