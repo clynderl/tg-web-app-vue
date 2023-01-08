9118d2db16a74df89f97a2ebe0511d10847b8818
Pavel Semenov <p.semenov@idaproject.com>
2023-01-08 16:05:53 +0700

    init

--- src/components/ExchangeCalculator.vue
+++ src/components/ExchangeCalculator.vue
@@ -8,16 +8,32 @@
       <input type="number" class="input">
 
     </div>
+    <p class="hint">Получаете</p>
+    <div class="input-box">
+      <div class="image-box">
+        <img :src="toActive?.logo?.simple" alt="">
+      </div>
+      <input type="number" class="input">
+
+    </div>
+    <div class="course">
+      <div class="title">Курс обмена</div>
+      <div class="value">
+        {{ course.from }} {{ fromActive.currency[0].toUpperCase() }} = {{ course.to }}
+        {{ toActive.currency[0].toUpperCase() }}
+      </div>
+    </div>
     <pre>
-    {{ fromActive }}
-    {{ toActive }}
-    {{ pairData }}
+<!--    {{ fromActive }}-->
+      <!--    {{ toActive }}-->
+      <!--    {{ pairData }}-->
+<!--      {{ course }}-->
     </pre>
   </div>
 </template>
 
 <script setup async>
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 
 const props = defineProps({
   fromActive: {
@@ -29,10 +45,25 @@ const props = defineProps({
     default: () => ({}),
   },
 });
-
 const pairData = ref(null);
 const pairDataResponse = await fetch(`https://dev7d8d3h4.sova.gg/api/v1/calculator/pair/${props.fromActive.ids[0]}/${props.toActive.ids[0]}/`);
 pairData.value = await pairDataResponse.json();
+const roundNumber = (n, decimals = 0) => Number(`${Math.round(`${n}e${decimals}`)}e-${decimals}`);
+const course = computed(() => {
+  return pairData.value.course > 1 ? {
+    from: 1,
+    to: roundNumber(pairData.value?.course, pairData.value?.to?.round),
+  } : {from: roundNumber(1 / pairData.value?.course, pairData.value?.from?.round), to: 1};
+});
+
+// const ranges = computed(() => {
+//   return {
+//     min: pairData.value?.from?.max ? pairData.value?.from?.min : pairData.value?.to?.min / pairData.value?.course,
+//     max: pairData.value?.from?.max,
+//   };
+// });
+// rate() {
+// return this.course > 1 ? { from: 1, to: roundNumber(this.course, this.round.to) } : { from: roundNumber(1 / this.course, this.round.from), to: 1 };
 </script>
 
 <style scoped>
@@ -83,5 +114,12 @@ pairData.value = await pairDataResponse.json();
   letter-spacing: 0.01em;
   animation-duration: 10ms;
   -webkit-tap-highlight-color: transparent;
+}
+
+.course {
+  display: flex;
+  justify-content: space-between;
+  font-size: 14px;
+  color: var(--tg-theme-hint-color);
 }
 </style>
